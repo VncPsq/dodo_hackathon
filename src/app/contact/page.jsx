777528732e0fccf43070d71db87f9bb25f79fe2b
Vincent Pasquiou <vincent.pasquiou@gmail.com>
2024-07-02 15:22:01 +0200

    team dodo is spleeping !

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,7 @@
+"use clients";
+
+function Contact() {
+  return <h1>Contact la team dodo</h1>;
+}
+
+export default Contact;
---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <>
+      <div className="bg-amber-400">
+        <h1>Bonjour</h1>
+        <p>Lorem ipsum.</p>
+      </div>
+    </>
+  );
+}
